7fc56c40654762634b7b703feba47f57af6302a2
Samin Yasar <34398984+saminyasareram444@users.noreply.github.com>
2023-03-27 02:21:26 +0600

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+const Contact = () => {
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+    text-align: center;
+
+    .container {
+      margin-top: 6rem;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact page</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed/v1/place?q=South+Paikpara+-+Pirerbag+Amtola+Road,+Dhaka,+Bangladesh&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        title="myMap"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xeqdgwnq"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
